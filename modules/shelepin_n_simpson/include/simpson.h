b19399fef863c50105c8cc772ed70b10d3fea899
Nick Shell <kiwi0tails@gmail.com>
2022-04-30 19:49:33 +0300

    cleaned up

--- modules/shelepin_n_simpson/include/simpson.h
+++ modules/shelepin_n_simpson/include/simpson.h
@@ -1,29 +1,43 @@
 // Copyright 2022 Shelepin Nikita
 
-#ifndef MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
-#define MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
+#ifndef MODULES_SHELEPIN_N_SIMPSON_INCLUDE_SIMPSON_H_
+#define MODULES_SHELEPIN_N_SIMPSON_INCLUDE_SIMPSON_H_
 
-class ComplexNumber {
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <vector>
+
+using std::function;
+using std::pair;
+using std::vector;
+
+class SimpsonSolver {
  public:
-    ComplexNumber();
-    ComplexNumber(const double real, const double imag);
-    ComplexNumber(const ComplexNumber& z);
-    ComplexNumber& operator=(const ComplexNumber& z);
-    double getRe() const;
-    double getIm() const;
-    void setRe(const double real);
-    void setIm(const double imag);
-    ComplexNumber operator + (const ComplexNumber& z) const;
-    ComplexNumber operator - (const ComplexNumber& z) const;
-    ComplexNumber operator * (const ComplexNumber& z) const;
-    ComplexNumber operator / (const ComplexNumber& z) const;
-    bool operator == (const ComplexNumber& z) const;
-    bool operator != (const ComplexNumber& z) const;
+  SimpsonSolver();
+  SimpsonSolver(const function<double(vector<double>)> f,
+                const vector<pair<double, double>> limits, const vector<int> n);
+  SimpsonSolver(const SimpsonSolver& other);
+
+  SimpsonSolver& operator=(const SimpsonSolver& other);
+
+  function<double(vector<double>)> getFunction() const;
+  vector<pair<double, double>> getLimits() const;
+  vector<int> getN() const;
+
+  void setFunction(const function<double(vector<double>)> f);
+  void setLimits(const vector<pair<double, double>> limits);
+  void setN(vector<int> n);
+
+  bool operator==(const SimpsonSolver& other) const;
+  bool operator!=(const SimpsonSolver& other) const;
+
+  double solve();
 
  private:
-    bool equalsZero(const ComplexNumber& z) const;
-    double real_;
-    double imaginary_;
+  function<double(vector<double>)> f;
+  vector<pair<double, double>> limits;
+  vector<int> n;
 };
 
-#endif  // MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
+#endif  // MODULES_SHELEPIN_N_SIMPSON_INCLUDE_SIMPSON_H_
--- modules/shelepin_n_simpson/src/simpson.cpp
+++ modules/shelepin_n_simpson/src/simpson.cpp
@@ -2,97 +2,40 @@
 
 #include "include/simpson.h"
 
+SimpsonSolver::SimpsonSolver() {}
 
-const double eps = std::numeric_limits<double>::epsilon();
+SimpsonSolver::SimpsonSolver(const function<double(vector<double>)> f,
+                             const vector<pair<double, double>> limits,
+                             const vector<int> n) {}
 
-ComplexNumber::ComplexNumber() : real_(0), imaginary_(0) {}
+SimpsonSolver::SimpsonSolver(const SimpsonSolver& other) {}
 
-ComplexNumber::ComplexNumber(const double real,
-                             const double imaginary)
-                             : real_(real), imaginary_(imaginary) {}
-
-ComplexNumber::ComplexNumber(const ComplexNumber& z)
-                             : real_(z.getRe()), imaginary_(z.getIm()) {}
-
-ComplexNumber& ComplexNumber::operator=(const ComplexNumber& z) {
-    real_ = z.getRe();
-    imaginary_ = z.getIm();
-
-    return *this;
+SimpsonSolver& SimpsonSolver::operator=(const SimpsonSolver& other) {
+  return SimpsonSolver();
 }
 
-double ComplexNumber::getRe() const {
-    return real_;
+function<double(vector<double>)> SimpsonSolver::getFunction() const {
+  return function<double(vector<double>)>();
 }
 
-double ComplexNumber::getIm() const {
-    return imaginary_;
+vector<pair<double, double>> SimpsonSolver::getLimits() const {
+  return vector<pair<double, double>>();
 }
 
-void ComplexNumber::setRe(const double real) {
-    real_ = real;
+vector<int> SimpsonSolver::getN() const { return vector<int>(); }
+
+void SimpsonSolver::setFunction(const function<double(vector<double>)> f) {}
+
+void SimpsonSolver::setLimits(const vector<pair<double, double>> limits) {}
+
+void SimpsonSolver::setN(vector<int> n) {}
+
+bool SimpsonSolver::operator==(const SimpsonSolver& other) const {
+  return false;
 }
 
-void ComplexNumber::setIm(const double imaginary) {
-    imaginary_ = imaginary;
+bool SimpsonSolver::operator!=(const SimpsonSolver& other) const {
+  return false;
 }
 
-ComplexNumber ComplexNumber::operator+(const ComplexNumber& z) const {
-    ComplexNumber sum;
-
-    sum.setRe(this->getRe() + z.getRe());
-    sum.setIm(this->getIm() + z.getIm());
-
-    return sum;
-}
-
-ComplexNumber ComplexNumber::operator-(const ComplexNumber& z) const {
-    ComplexNumber difference;
-
-    difference.setRe(this->getRe() - z.getRe());
-    difference.setIm(this->getIm() - z.getIm());
-
-    return difference;
-}
-
-ComplexNumber ComplexNumber::operator*(const ComplexNumber& z) const {
-    ComplexNumber multiplication;
-
-    multiplication.setRe(this->getRe() * z.getRe() -
-                         this->getIm() * z.getIm());
-    multiplication.setIm(this->getRe() * z.getIm() +
-                         this->getIm() * z.getRe());
-
-    return multiplication;
-}
-
-bool ComplexNumber::equalsZero(const ComplexNumber& z) const {
-    return z.getRe() < eps && z.getRe() > -eps &&
-           z.getIm() < eps && z.getIm() > -eps;
-}
-
-ComplexNumber ComplexNumber::operator/(const ComplexNumber& z) const {
-    ComplexNumber division;
-
-    if (equalsZero(z)) {
-        throw std::string("Can't divide by zero");
-    } else {
-        double denominator = 1.0 / (z.getRe() * z.getRe() +
-                                    z.getIm() * z.getIm());
-
-        division.setRe(denominator * this->getRe() * z.getRe() +
-                       denominator * this->getIm() * z.getIm());
-        division.setIm(denominator * this->getIm() * z.getRe() -
-                       denominator * this->getRe() * z.getIm());
-    }
-
-    return division;
-}
-
-bool ComplexNumber::operator == (const ComplexNumber& z) const {
-    return equalsZero(*this - z);
-}
-
-bool ComplexNumber::operator != (const ComplexNumber& z) const {
-    return !(*this == z);
-}
+double SimpsonSolver::solve() { return 0.0; }
