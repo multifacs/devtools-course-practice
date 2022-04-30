f4e76e896782970a930222ee47b018f456e9479f
Nick Shell <kiwi0tails@gmail.com>
2022-04-30 23:37:03 +0300

    default constructor test

--- modules/shelepin_n_simpson/test/test_simpson.cpp
+++ modules/shelepin_n_simpson/test/test_simpson.cpp
@@ -11,4 +11,16 @@
 
 TEST(Shelepin_Nikita_SimpsonSolverTest, DefaultConstructor) {
   ASSERT_NO_THROW(SimpsonSolver());
+
+  SimpsonSolver a;
+  vector<int> test_n = {1};
+  vector<pair<double, double>> test_limits = {{1, 2}};
+  function<double(vector<double>)> test_func = [](vector<double> vec) {
+    double x = vec[0];
+    return x;
+  };
+
+  ASSERT_EQ(a.getN(), test_n);
+  ASSERT_EQ(a.getLimits(), test_limits);
+  ASSERT_EQ(a.getFunction()({100}), test_func({100}));
 }
