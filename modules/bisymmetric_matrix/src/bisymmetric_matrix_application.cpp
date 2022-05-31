0e4b00e80387f7561468535bc672d72717aeb1d5
Nick Shell <kiwi0tails@gmail.com>
2022-05-31 18:47:55 +0300

    styling

--- modules/bisymmetric_matrix/src/bisymmetric_matrix_application.cpp
+++ modules/bisymmetric_matrix/src/bisymmetric_matrix_application.cpp
@@ -24,7 +24,6 @@ std::string BMApplication::operator()(int argc, const char** argv) {
   int i = 1;
 
   if (std::string(argv[i]) == "--test1" || std::string(argv[i]) == "-t1") {
-    int test_size = 2;
     std::vector<std::vector<double>> test_matrix = {{1, 2}, {2, 1}};
     BisymmetricMatrix a(test_matrix);
     BisymmetricMatrix b;
